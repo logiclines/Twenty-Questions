ec56b81782df90ee54595c73c8c8dbb75ff108fb
Pavan Nagendra <jjjpav@gmail.com>
2018-06-06 23:36:48 +0530

    Update Twenty-Questions.py

--- Twenty-Questions.py
+++ Twenty-Questions.py
@@ -5,6 +5,8 @@ i = 1;
 n = ['Marilyn Monroe', 'Abraham Lincoln', 'Mother Teresa', 'John F. Kennedy', 'Martin Luther King', 'Nelson Mandela','Winston Churchill', 'Bill gates', 'Mahatma Gandhi', 'Margaret Thatcher','Christopher Columbus', 'Charles Darwin', 'Elvis Presley','Albert Einstein','Paul McCartney','Mikhail Gorbachev', 'Jawaharlal Nehru', 'Leonardo Da Vinci', 'Leo Tolstoy', 'Pablo Picasso', 'Vincent Van Gogh','Louis Pasteur','Thomas Edison','Indira Gandhi', 'Oprah Winfrey', 'Benazir Bhutto', 'Ludwig Beethoven']
 
 randomList = random.sample(n,8)
+# The 8 in random.sample(n,8) is to be able to represent a word list in binary
+# 000; 001; 010; 011; 100; 101; 110; 111
 
 print randomList
 print 'Type your selection exactly as shown in the list above.'
